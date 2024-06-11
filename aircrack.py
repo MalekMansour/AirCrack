e332634685ce9c86847548480d433ae9717b0ee4
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-06-11 00:28:59 -0600

    Update aircrack.py

--- aircrack.py
+++ aircrack.py
@@ -51,6 +51,7 @@ def show_info():
     cipher = selected_network.cipher
     print(f"Network Info:\nSSID: {ssid}\nBSSID: {bssid}\nSignal: {signal}\nFrequency: {freq} MHz\nAKM: {akm}\nCipher: {cipher}")
 
+# Get Default Gateway
 def get_default_gateway():
     try:
         result = subprocess.run(["ipconfig"], capture_output=True, text=True)
